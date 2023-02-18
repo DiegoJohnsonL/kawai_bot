0b1238ba19f6714f62db84c9b7eddad2280967b2
Diego Johnson <diegojohnson46@gmail.com>
2023-02-18 09:13:06 -0500

    v0.1

---
+++ lib/colors.dart
@@ -0,0 +1,5 @@
+import 'dart:ui';
+
+const bgColor = Color(0xff2d9898);
+const textColor = Color(0xfffEFDFC);
+const chatBgColor = Color(0xff202225);
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
+import 'package:kawai_bot/colors.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,10 +11,91 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  SpeechToText speechToText = SpeechToText();
+
+  var text = "Hold the button and start speaking ";
+  var isListening = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: AvatarGlow(
+        endRadius: 75.0,
+        animate: isListening,
+        duration: const Duration(milliseconds: 2000),
+        glowColor: bgColor,
+        repeat: true,
+        repeatPauseDuration: const Duration(milliseconds: 100),
+        showTwoGlows: true,
+        child: GestureDetector(
+          onTapDown: (details) async {
+            if (!isListening) {
+              var available = await speechToText.initialize();
+              if (available) {
+                setState(() {
+                  isListening = true;
+                  speechToText.listen(onResult: (result) {
+                    setState(() {
+                      text = result.recognizedWords;
+                    });
+                  });
+                });
+              }
+            }
+          },
+          onTapUp: (details) {
+            setState(() {
+              isListening = false;
+            });
+            speechToText.stop();
+          },
+          child: CircleAvatar(
+            backgroundColor: bgColor,
+            radius: 35,
+            child: Icon(
+              isListening ? Icons.mic : Icons.mic_none,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+      appBar: AppBar(
+        leading: const Icon(Icons.sort_rounded, color: Colors.white),
+        centerTitle: true,
+        backgroundColor: bgColor,
+        elevation: 0.0,
+        title: const Text(
+          "Kawai Assistant uwu",
+          style: TextStyle(fontWeight: FontWeight.w600, color: textColor),
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+        child: Column(
+          children: [
+            Text(
+              text,
+              style: TextStyle(
+                  fontSize: 24,
+                  color: isListening ? Colors.black87 : Colors.black54,
+                  fontWeight: FontWeight.w600),
+            ),
+            const SizedBox(height: 12),
+            Expanded(
+                child: Container(
+              decoration: BoxDecoration(
+                  color: chatBgColor, borderRadius: BorderRadius.circular(12)),
+            )),
+            const SizedBox(height: 12),
+            const Text("Developed by kawai boys",
+                style: TextStyle(
+                    color: Colors.black54,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16))
+          ],
+        ),
+      ),
     );
   }
 }
